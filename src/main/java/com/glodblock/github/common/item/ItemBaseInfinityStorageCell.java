3bed98889b4694719a543150fad392c7287e3b7c
Zereff06 <Zereff06@gmail.com>
2024-07-01 22:24:23 +0300

    code improvement for water cell 3 * changed value to Integer.MAX_VALUE * 1_000_000L for creative cells

--- src/main/java/com/glodblock/github/common/item/ItemBaseInfinityStorageCell.java
+++ src/main/java/com/glodblock/github/common/item/ItemBaseInfinityStorageCell.java
@@ -1,11 +1,22 @@
 package com.glodblock.github.common.item;
 
+import java.util.List;
+
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
 
 import com.glodblock.github.api.FluidCraftAPI;
+import com.glodblock.github.common.storage.IFluidCellInventory;
+import com.glodblock.github.common.storage.IFluidCellInventoryHandler;
 import com.glodblock.github.common.storage.IStorageFluidCell;
+import com.glodblock.github.util.NameConst;
 
+import appeng.api.AEApi;
+import appeng.api.storage.IMEInventoryHandler;
+import appeng.api.storage.StorageChannel;
 import appeng.api.storage.data.IAEFluidStack;
 import appeng.items.AEBaseItem;
 import appeng.items.contents.CellUpgrades;
@@ -14,12 +25,12 @@ public abstract class ItemBaseInfinityStorageCell extends AEBaseItem implements 
 
     @Override
     public long getBytes(ItemStack cellItem) {
-        return Integer.MAX_VALUE;
+        return 0;
     }
 
     @Override
     public int getBytesPerType(ItemStack cellItem) {
-        return 1;
+        return 0;
     }
 
     @Override
@@ -27,9 +38,6 @@ public abstract class ItemBaseInfinityStorageCell extends AEBaseItem implements 
         return requestedAddition == null || requestedAddition.getFluid() == null
                 || FluidCraftAPI.instance().isBlacklistedInStorage(requestedAddition.getFluid().getClass());
     }
-
-    @Override
-    public abstract IInventory getConfigInventory(ItemStack is);
 
     @Override
     public boolean storableInStorageCell() {
@@ -54,6 +62,32 @@ public abstract class ItemBaseInfinityStorageCell extends AEBaseItem implements 
     @Override
     public IInventory getUpgradesInventory(ItemStack is) {
         return new CellUpgrades(is, 0);
+    }
+
+    @Override
+    public void addCheckedInformation(final ItemStack stack, final EntityPlayer player, final List<String> lines,
+                                      final boolean displayMoreInfo) {
+        final IMEInventoryHandler<?> inventory = AEApi.instance().registries().cell()
+            .getCellInventory(stack, null, StorageChannel.FLUIDS);
+
+        if (inventory instanceof final IFluidCellInventoryHandler handler) {
+            final IFluidCellInventory cellInventory = handler.getCellInv();
+
+            if (GuiScreen.isCtrlKeyDown()) {
+                if (!cellInventory.getContents().isEmpty()) {
+                    lines.add(StatCollector.translateToLocal(NameConst.TT_CELL_CONTENTS));
+                    for (IAEFluidStack fluid : cellInventory.getContents()) {
+                        if (fluid != null) {
+                            lines.add(String.format("  %s %s", StatCollector.translateToLocal(NameConst.TT_INFINITY_FLUID_STORAGE_TIPS),  fluid.getFluidStack().getLocalizedName()));
+                        }
+                    }
+                } else {
+                    lines.add(StatCollector.translateToLocal(NameConst.TT_CELL_EMPTY));
+                }
+            } else {
+                lines.add(StatCollector.translateToLocal(NameConst.TT_CTRL_FOR_MORE));
+            }
+        }
     }
 
 }
--- src/main/java/com/glodblock/github/common/item/ItemInfinityWaterStorageCell.java
+++ src/main/java/com/glodblock/github/common/item/ItemInfinityWaterStorageCell.java
@@ -3,27 +3,17 @@ package com.glodblock.github.common.item;
 import static com.glodblock.github.util.Util.FluidUtil.water_bucket;
 
 import java.util.EnumSet;
-import java.util.List;
 
-import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.StatCollector;
 
 import com.glodblock.github.FluidCraft;
-import com.glodblock.github.common.storage.IFluidCellInventory;
-import com.glodblock.github.common.storage.IFluidCellInventoryHandler;
 import com.glodblock.github.common.storage.IStorageFluidCell;
 import com.glodblock.github.common.tabs.FluidCraftingTabs;
 import com.glodblock.github.loader.IRegister;
 import com.glodblock.github.util.NameConst;
 
-import appeng.api.AEApi;
 import appeng.api.config.FuzzyMode;
-import appeng.api.storage.IMEInventoryHandler;
-import appeng.api.storage.StorageChannel;
-import appeng.api.storage.data.IAEFluidStack;
 import appeng.core.features.AEFeature;
 import appeng.tile.inventory.AppEngInternalInventory;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -53,32 +43,6 @@ public class ItemInfinityWaterStorageCell extends ItemBaseInfinityStorageCell
         setTextureName(FluidCraft.resource(NameConst.ITEM_INFINITY_FLUID_STORAGE).toString());
         this.setFeature(EnumSet.of(AEFeature.StorageCells));
         this.setMaxStackSize(1);
-    }
-
-    @Override
-    public void addCheckedInformation(final ItemStack stack, final EntityPlayer player, final List<String> lines,
-                                      final boolean displayMoreInfo) {
-        final IMEInventoryHandler<?> inventory = AEApi.instance().registries().cell()
-            .getCellInventory(stack, null, StorageChannel.FLUIDS);
-
-        if (inventory instanceof final IFluidCellInventoryHandler handler) {
-            final IFluidCellInventory cellInventory = handler.getCellInv();
-
-            if (GuiScreen.isCtrlKeyDown()) {
-                if (!cellInventory.getContents().isEmpty()) {
-                    lines.add(StatCollector.translateToLocal(NameConst.TT_CELL_CONTENTS));
-                    for (IAEFluidStack fluid : cellInventory.getContents()) {
-                        if (fluid != null) {
-                            lines.add(String.format("  %s %s", StatCollector.translateToLocal(NameConst.TT_INFINITY_FLUID_STORAGE_TIPS),  fluid.getFluidStack().getLocalizedName()));
-                        }
-                    }
-                } else {
-                    lines.add(StatCollector.translateToLocal(NameConst.TT_CELL_EMPTY));
-                }
-            } else {
-                lines.add(StatCollector.translateToLocal(NameConst.TT_CTRL_FOR_MORE));
-            }
-        }
     }
 
     @Override
