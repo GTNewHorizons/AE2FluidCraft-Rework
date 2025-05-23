dcac065134d65e24b9e79828de6c14b9769bbf90
Kogepan229 <kogepan25629@gmail.com>
2025-05-24 02:29:09 +0900

    simple

--- src/main/java/com/glodblock/github/crossmod/waila/part/SpeedWailaDataProvider.java
+++ src/main/java/com/glodblock/github/crossmod/waila/part/SpeedWailaDataProvider.java
@@ -7,8 +7,6 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
-import com.glodblock.github.common.parts.PartFluidExportBus;
-import com.glodblock.github.common.parts.PartFluidImportBus;
 import com.glodblock.github.common.parts.base.FCSharedFluidBus;
 import com.glodblock.github.crossmod.waila.Tooltip;
 
@@ -22,14 +20,8 @@ public class SpeedWailaDataProvider extends BasePartWailaDataProvider {
     @Override
     public List<String> getWailaBody(final IPart part, final List<String> currentToolTip,
             final IWailaDataAccessor accessor, final IWailaConfigHandler config) {
-        if (part instanceof PartFluidExportBus bus) {
-            PartFluidExportBus tmpBus = new PartFluidExportBus(bus.getItemStack());
-            tmpBus.readFromNBT(accessor.getNBTData());
-            currentToolTip.add(Tooltip.partFluidBusFormat(tmpBus.calculateAmountToSend() / 5));
-        } else if (part instanceof PartFluidImportBus bus) {
-            PartFluidImportBus tmpBus = new PartFluidImportBus(bus.getItemStack());
-            tmpBus.readFromNBT(accessor.getNBTData());
-            currentToolTip.add(Tooltip.partFluidBusFormat(tmpBus.calculateAmountToSend() / 5));
+        if (accessor.getNBTData().hasKey("BusSpeed")) {
+            currentToolTip.add(Tooltip.partFluidBusFormat(accessor.getNBTData().getInteger("BusSpeed")));
         }
         return currentToolTip;
     }
@@ -37,8 +29,8 @@ public class SpeedWailaDataProvider extends BasePartWailaDataProvider {
     @Override
     public NBTTagCompound getNBTData(final EntityPlayerMP player, final IPart part, final TileEntity te,
             final NBTTagCompound tag, final World world, final int x, final int y, final int z) {
-        if (part instanceof FCSharedFluidBus) {
-            part.writeToNBT(tag);
+        if (part instanceof FCSharedFluidBus bus) {
+            tag.setInteger("BusSpeed", bus.calculateAmountToSend() / 5);
         }
         return tag;
     }
