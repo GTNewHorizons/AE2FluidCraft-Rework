f694689a297fb10d18466d54d60389ad3bfc486f
Kogepan229 <kogepan25629@gmail.com>
2025-05-24 02:01:44 +0900

    Fix fluid import/export bus GUI when WAILA is disabled

--- src/main/java/com/glodblock/github/crossmod/waila/part/SpeedWailaDataProvider.java
+++ src/main/java/com/glodblock/github/crossmod/waila/part/SpeedWailaDataProvider.java
@@ -7,6 +7,8 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
+import com.glodblock.github.common.parts.PartFluidExportBus;
+import com.glodblock.github.common.parts.PartFluidImportBus;
 import com.glodblock.github.common.parts.base.FCSharedFluidBus;
 import com.glodblock.github.crossmod.waila.Tooltip;
 
@@ -20,9 +22,14 @@ public class SpeedWailaDataProvider extends BasePartWailaDataProvider {
     @Override
     public List<String> getWailaBody(final IPart part, final List<String> currentToolTip,
             final IWailaDataAccessor accessor, final IWailaConfigHandler config) {
-        if (part instanceof FCSharedFluidBus) {
-            part.readFromNBT(accessor.getNBTData());
-            currentToolTip.add(Tooltip.partFluidBusFormat(((FCSharedFluidBus) part).calculateAmountToSend() / 5));
+        if (part instanceof PartFluidExportBus bus) {
+            PartFluidExportBus tmpBus = new PartFluidExportBus(bus.getItemStack());
+            tmpBus.readFromNBT(accessor.getNBTData());
+            currentToolTip.add(Tooltip.partFluidBusFormat(tmpBus.calculateAmountToSend() / 5));
+        } else if (part instanceof PartFluidImportBus bus) {
+            PartFluidImportBus tmpBus = new PartFluidImportBus(bus.getItemStack());
+            tmpBus.readFromNBT(accessor.getNBTData());
+            currentToolTip.add(Tooltip.partFluidBusFormat(tmpBus.calculateAmountToSend() / 5));
         }
         return currentToolTip;
     }
