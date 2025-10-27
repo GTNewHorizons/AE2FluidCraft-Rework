024222d305213aee0880ec0c265f25cccc730827
Kogepan229 <kogepan25629@gmail.com>
2025-10-27 20:38:04 +0900

    Fix funny

--- src/main/java/com/glodblock/github/common/parts/PartFluidLevelEmitter.java
+++ src/main/java/com/glodblock/github/common/parts/PartFluidLevelEmitter.java
@@ -3,8 +3,9 @@ package com.glodblock.github.common.parts;
 import net.minecraft.item.ItemStack;
 
 import appeng.helpers.Reflected;
+import appeng.parts.automation.PartLevelEmitter;
 
-public class PartFluidLevelEmitter extends PartLevelTerminal {
+public class PartFluidLevelEmitter extends PartLevelEmitter {
 
     @Reflected
     public PartFluidLevelEmitter(final ItemStack is) {
