b4265992c3bf18086932b0295bafe5413aba27f8
lc-1337 <lordicocain@gmail.com>
2025-10-30 20:41:53 +0200

    io bus fixes

--- src/main/java/com/glodblock/github/common/parts/PartFluidPatternTerminal.java
+++ src/main/java/com/glodblock/github/common/parts/PartFluidPatternTerminal.java
@@ -2,43 +2,12 @@ package com.glodblock.github.common.parts;
 
 import net.minecraft.item.ItemStack;
 
-import com.glodblock.github.client.textures.FCPartsTexture;
-import com.glodblock.github.common.parts.base.FCPart;
+import appeng.parts.reporting.PartPatternTerminal;
 
-import appeng.core.sync.GuiBridge;
-
-public class PartFluidPatternTerminal extends FCPart {
-
-    private static final FCPartsTexture FRONT_BRIGHT_ICON = FCPartsTexture.PartFluidPatternTerminal_Bright;
-    private static final FCPartsTexture FRONT_DARK_ICON = FCPartsTexture.PartFluidPatternTerminal_Colored;
-    private static final FCPartsTexture FRONT_COLORED_ICON = FCPartsTexture.PartFluidPatternTerminal_Dark;
+public class PartFluidPatternTerminal extends PartPatternTerminal {
 
     public PartFluidPatternTerminal(ItemStack is) {
         super(is);
     }
 
-    @Override
-    public GuiBridge getGui() {
-        return GuiBridge.GUI_PATTERN_TERMINAL;
-    }
-
-    @Override
-    public FCPartsTexture getFrontBright() {
-        return FRONT_BRIGHT_ICON;
-    }
-
-    @Override
-    public FCPartsTexture getFrontColored() {
-        return FRONT_COLORED_ICON;
-    }
-
-    @Override
-    public FCPartsTexture getFrontDark() {
-        return FRONT_DARK_ICON;
-    }
-
-    @Override
-    public boolean isLightSource() {
-        return false;
-    }
 }
--- src/main/java/com/glodblock/github/common/parts/PartFluidPatternTerminalEx.java
+++ src/main/java/com/glodblock/github/common/parts/PartFluidPatternTerminalEx.java
@@ -2,43 +2,11 @@ package com.glodblock.github.common.parts;
 
 import net.minecraft.item.ItemStack;
 
-import com.glodblock.github.client.textures.FCPartsTexture;
-import com.glodblock.github.common.parts.base.FCPart;
+import appeng.parts.reporting.PartPatternTerminalEx;
 
-import appeng.core.sync.GuiBridge;
-
-public class PartFluidPatternTerminalEx extends FCPart {
-
-    private static final FCPartsTexture FRONT_BRIGHT_ICON = FCPartsTexture.PartFluidPatternTerminal_Bright;
-    private static final FCPartsTexture FRONT_DARK_ICON = FCPartsTexture.PartFluidPatternTerminal_Colored;
-    private static final FCPartsTexture FRONT_COLORED_ICON = FCPartsTexture.PartFluidPatternTerminal_Dark;
+public class PartFluidPatternTerminalEx extends PartPatternTerminalEx {
 
     public PartFluidPatternTerminalEx(ItemStack is) {
         super(is);
-    }
-
-    @Override
-    public GuiBridge getGui() {
-        return GuiBridge.GUI_PATTERN_TERMINAL_EX;
-    }
-
-    @Override
-    public FCPartsTexture getFrontBright() {
-        return FRONT_BRIGHT_ICON;
-    }
-
-    @Override
-    public FCPartsTexture getFrontColored() {
-        return FRONT_COLORED_ICON;
-    }
-
-    @Override
-    public FCPartsTexture getFrontDark() {
-        return FRONT_DARK_ICON;
-    }
-
-    @Override
-    public boolean isLightSource() {
-        return false;
     }
 }
