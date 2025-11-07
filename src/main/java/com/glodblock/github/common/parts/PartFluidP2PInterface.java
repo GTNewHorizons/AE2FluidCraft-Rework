b3dc12ec3dfd07e99a0e09d1686dfd2822967f0c
hiroscho <hiroscho@users.noreply.github.com>
2025-11-07 18:52:19 +0100

    Fix dupe bug for dual p2p interface (#352)
    (cherry picked from commit 488b08f1fa87ee21f112456f3853a992e92c6d53)

--- src/main/java/com/glodblock/github/common/parts/PartFluidP2PInterface.java
+++ src/main/java/com/glodblock/github/common/parts/PartFluidP2PInterface.java
@@ -230,6 +230,7 @@ public class PartFluidP2PInterface extends PartP2PTunnelStatic<PartFluidP2PInter
                     Platform.spawnDrops(te.getWorldObj(), te.xCoord, te.yCoord, te.zCoord, drops);
                 }
                 duality.setStorage(p2p.duality.getStorage());
+                duality.setSlotInv(p2p.duality.getSlotInv());
                 duality.sharedInventory = true;
             } else {
                 if (duality.sharedInventory) {
