400d2350a06854d5cdf58007a6e15e140d231da8
danyadev <danyadev@mail.ru>
2025-11-08 15:10:05 +0300

    fix fluid rendering in a slot after a water bucket

--- src/main/java/com/glodblock/github/client/render/ItemCertusQuartzTankRender.java
+++ src/main/java/com/glodblock/github/client/render/ItemCertusQuartzTankRender.java
@@ -52,6 +52,7 @@ public class ItemCertusQuartzTankRender implements IItemRenderer {
         GL11.glPushAttrib(GL11.GL_ENABLE_BIT);
         GL11.glDisable(GL11.GL_CULL_FACE);
         GL11.glEnable(GL11.GL_LIGHTING);
+        GL11.glEnable(GL11.GL_ALPHA_TEST);
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
