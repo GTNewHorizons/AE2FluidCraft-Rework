af0434a01c4bf132b35c9e763e6965c3d68ffeb1
Kogepan229 <kogepan25629@gmail.com>
2026-01-17 00:57:29 +0900

    Revert "remove transformers (#389)"
    This reverts commit e62215f02b4928cc8a7fe5bf38705af700774d6c.

---
+++ src/main/java/com/glodblock/github/coremod/FCClassTransformer.java
@@ -0,0 +1,55 @@
+package com.glodblock.github.coremod;
+
+import net.minecraft.launchwrapper.IClassTransformer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+
+import com.glodblock.github.coremod.transform.ExternalStorageRegistryTransformer;
+import com.glodblock.github.coremod.transform.GuiCraftingTransformer;
+import com.glodblock.github.coremod.transform.NEITransformer;
+import com.gtnewhorizon.gtnhlib.asm.ASMUtil;
+
+public class FCClassTransformer implements IClassTransformer {
+
+    private static final Logger logger = LogManager.getLogger("ASM AE2FC");
+
+    @Override
+    public byte[] transform(String name, String transformedName, byte[] basicClass) {
+        ClassMapper tform;
+        switch (transformedName) {
+            case "appeng.client.gui.implementations.GuiCraftingCPU", "appeng.client.gui.implementations.GuiCraftConfirm", "net.p455w0rd.wirelesscraftingterminal.client.gui.GuiCraftConfirm", "appeng.client.gui.widgets.GuiCraftingTree" -> tform = GuiCraftingTransformer.INSTANCE;
+            case "appeng.integration.modules.NEI" -> tform = NEITransformer.INSTANCE;
+            case "appeng.core.features.registries.ExternalStorageRegistry" -> tform = ExternalStorageRegistryTransformer.INSTANCE;
+            default -> {
+                return basicClass;
+            }
+        }
+        logger.debug("Transforming class: " + transformedName);
+        final byte[] bytes = tform.transformClass(basicClass);
+        if (FluidCraftCore.DUMP_CLASSES()) {
+            ASMUtil.saveAsRawClassFile(basicClass, transformedName + "_PRE", this);
+            ASMUtil.saveAsRawClassFile(bytes, transformedName + "_POST", this);
+        }
+        return bytes;
+    }
+
+    public abstract static class ClassMapper {
+
+        public final byte[] transformClass(byte[] code) {
+            ClassReader reader = new ClassReader(code);
+            ClassWriter writer = new ClassWriter(reader, getWriteFlags());
+            reader.accept(getClassMapper(writer), ClassReader.EXPAND_FRAMES);
+            return writer.toByteArray();
+        }
+
+        protected int getWriteFlags() {
+            return 0;
+        }
+
+        protected abstract ClassVisitor getClassMapper(ClassVisitor downstream);
+    }
+}
