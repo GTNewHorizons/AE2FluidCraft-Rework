ae2e402fee3aa6eea2d93b710762955c623e9324
Kogepan229 <kogepan25629@gmail.com>
2026-01-17 22:41:58 +0900

    Delete transformers

--- src/main/java/com/glodblock/github/coremod/hooker/CoreModHooksClient.java
+++
@@ -1,9 +0,0 @@
-package com.glodblock.github.coremod.hooker;
-
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-
-@SideOnly(Side.CLIENT)
-public class CoreModHooksClient {
-
-}
