5426389e1e1ddfc31047ca08c03f23bd2ebaabcd
lc-1337 <lordicocain@gmail.com>
2026-05-08 10:27:05 +0300

    DEPRECATED

--- src/main/java/com/glodblock/github/common/item/ItemFluidStorageMonitor.java
+++ src/main/java/com/glodblock/github/common/item/ItemFluidStorageMonitor.java
@@ -1,5 +1,7 @@
 package com.glodblock.github.common.item;
 
+import java.util.List;
+
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -49,6 +51,13 @@ public class ItemFluidStorageMonitor extends FCBaseItem implements IPartItem {
     @SideOnly(Side.CLIENT)
     public int getSpriteNumber() {
         return 0;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, EntityPlayer player, List<String> toolTip, boolean flag) {
+        toolTip.add("§4DEPRECATED!");
+        super.addInformation(stack, player, toolTip, flag);
     }
 
     @Override
