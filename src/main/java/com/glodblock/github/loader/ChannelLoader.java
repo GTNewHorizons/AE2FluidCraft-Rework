6c148a470f411599aa1e81532e965627c9824362
Inphysible <103602207+Inphysible@users.noreply.github.com>
2026-06-12 08:00:05 -0400

    Resolve conversation: Removed dependency for `CPacketSuperStockReplenisher`

--- src/main/java/com/glodblock/github/loader/ChannelLoader.java
+++ src/main/java/com/glodblock/github/loader/ChannelLoader.java
@@ -6,7 +6,6 @@ import com.glodblock.github.network.CPacketDumpTank;
 import com.glodblock.github.network.CPacketFluidPatternTermBtns;
 import com.glodblock.github.network.CPacketLevelMaintainer;
 import com.glodblock.github.network.CPacketLevelTerminalCommands;
-import com.glodblock.github.network.CPacketSuperStockReplenisherUpdate;
 import com.glodblock.github.network.CPacketSwitchGuis;
 import com.glodblock.github.network.CPacketValueConfig;
 import com.glodblock.github.network.SPacketCustomButtonUpdate;
@@ -40,12 +39,6 @@ public class ChannelLoader implements Runnable {
                 SPacketSuperStockReplenisherUpdate.class,
                 id++,
                 Side.CLIENT);
-        netHandler.registerMessage(
-                new CPacketSuperStockReplenisherUpdate.Handler(),
-                CPacketSuperStockReplenisherUpdate.class,
-                id++,
-                Side.SERVER);
-
         netHandler.registerMessage(new CPacketValueConfig.Handler(), CPacketValueConfig.class, id++, Side.SERVER);
         netHandler
                 .registerMessage(new CPacketLevelMaintainer.Handler(), CPacketLevelMaintainer.class, id++, Side.SERVER);
